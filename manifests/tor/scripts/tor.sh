30b173523382cf7a489a556909bd79d43cb165ca
pjames <pjames@gmail.com>
2023-08-24 07:40:43 +0700

    Changed dir permission

--- manifests/tor/scripts/tor.sh
+++ manifests/tor/scripts/tor.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+SVC_DIR=/tor/hidden_services/services
+
 mkdir -p /tor/data
-mkdir -p /tor/hidden_services/services
+mkdir -p ${SVC_DIR}
+
+chmod -R 700 ${SVC_DIR}
 
 tor -f /configs/torrc
